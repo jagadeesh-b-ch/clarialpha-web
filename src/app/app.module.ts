9137ea1769561cb707cf84cb65fd94f47c9dc50e
jagadeesh-b-ch <jagadeesh7.chitta@gmail.com>
2017-03-16 09:23:04 +0530

    added create-article page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { CreateArticleComponent } from './create-article/create-article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateArticleComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot([
+      {
+        path: 'create-article',
+        component: CreateArticleComponent
+      },
+      {
+        path: '',
+        component: AppComponent,
+        pathMatch: 'full'
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/create-article/create-article.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './create-article.component.html'
+})
+export class CreateArticleComponent{
+    title = 'This is create article page';
+}
