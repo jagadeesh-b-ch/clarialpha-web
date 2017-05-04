3164a54f9348a331d34486449b6af44310e6105f
jagadeesh-b-ch@ofc <jagadeesh7.chitta@gmail.com>
2017-05-04 12:47:33 +0530

    removed unnecessary files in working build

--- source/app.component.ts
+++ source/app.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'This is a message from Angular 2. Hello world.!!';
+  title = 'This is a message from Angular 2. Hello world.!! and there is your change';
 }
