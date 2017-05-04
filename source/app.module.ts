3999261fef2b3ab9853f272ea11eb78309ece45e
jagadeesh-b-ch@ofc <jagadeesh7.chitta@gmail.com>
2017-05-04 18:52:50 +0530

    material design included

--- source/app.module.ts
+++ source/app.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'hammerjs';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { CreateArticleComponent } from './create-article/create-article.component';
@@ -15,6 +19,9 @@ import { CreateArticleComponent } from './create-article/create-article.componen
 	imports: [
 		BrowserModule,
 		FormsModule,
+		BrowserAnimationsModule,
+		MdButtonModule,
+		MdCheckboxModule,
 		RouterModule.forRoot([
 		{
 			path: 'create-article',
