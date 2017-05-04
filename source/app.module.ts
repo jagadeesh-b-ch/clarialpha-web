b2b2396312be581cb54e626aee96b8b4f64443e9
jagadeesh-b-ch@ofc <jagadeesh7.chitta@gmail.com>
2017-05-04 17:38:51 +0530

    routing added while using webpack bundler

--- source/app.module.ts
+++ source/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { FormsModule } from '@angular/forms';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CreateArticleComponent } from './create-article/create-article.component';
@@ -13,7 +14,18 @@ import { CreateArticleComponent } from './create-article/create-article.componen
 	],
 	imports: [
 		BrowserModule,
-		FormsModule
+		FormsModule,
+		RouterModule.forRoot([
+		{
+			path: 'create-article',
+			component: CreateArticleComponent
+		},
+		{
+			path: '',
+			redirectTo: '/create-article',
+			pathMatch: 'full'
+		}
+		])
 	],
 	providers: [],
 	bootstrap: [
