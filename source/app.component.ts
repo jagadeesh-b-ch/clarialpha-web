0a5f99d4b8968681641ec3d83f9149a282fc5a4f
jagadeesh-b-ch <jagadeesh7.chitta@gmail.com>
2018-03-27 22:12:14 +0530

    converted project from angular to react

--- source/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
-})
-export class AppComponent {
-  title = 'This is a message from Angular 2. Hello world.!! and there is your change';
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,26 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-const webpack = require('webpack'); //to access built-in plugins
-//var helpers = require('./helpers');
-
-module.exports = {
-  entry:{
-	'polyfills':'./dependencies/polyfills.ts',
-	'app':'./source/app.module.ts'
-  },
-  output: {
-    filename: '[name].js',
-  },
-  resolve: {
-    extensions: ['.js', '.ts']
-  },
-  module: {
-	  rules: [
-                {
-					test: /\.ts$/,
-					loaders: ['awesome-typescript-loader', 'angular2-template-loader']
-				},
-				{
-					test: /\.html$/,
-					loader: 'html-loader'
-				},
-				{
-					test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-					loader: 'file?name=assets/[name].[hash].[ext]'
-				},
-				{
-				   test: /\.css$/,
-				   loaders: ['to-string-loader', 'css-loader']
-				}
-	]
-  },
-  plugins: [
-    // Workaround for angular/angular#11580
-	/*new webpack.ContextReplacementPlugin(
-		// The (\\|\/) piece accounts for path separators in *nix and Windows
-		/angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
-		helpers.root('./source'), // location of your src
-		{} // a map of your routes
-	),*/
-    new webpack.optimize.UglifyJsPlugin(),
-	new webpack.optimize.CommonsChunkPlugin({
-		name: ['app', 'polyfills']
-	}),
-    new HtmlWebpackPlugin({template: './index.html'})
-  ]
-};
+var config = {
+    entry: './main.js',
+    output: {
+       path:'/',
+       filename: 'index.js',
+    },
+    devServer: {
+       inline: true,
+       port: 8080
+    },
+    module: {
+       rules: [
+          {
+             test: /\.jsx?$/,
+             exclude: /node_modules/,
+             use: {
+                 loader: 'babel-loader',
+                 query: {
+                    presets: ['es2015', 'react']
+                 }
+             }
+          }
+       ]
+    }
+ }
+ module.exports = config;
