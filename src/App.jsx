6dfeb3c1dcd396fd96ba7c8890ffa620055f9980
jagadeesh-b-ch <jagadeesh7.chitta@gmail.com>
2018-05-27 17:32:38 +0530

    configured how the style sheets are used in the application

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './styles/global-style.scss';
+
+class App extends React.Component {
+   render() {
+      return (
+          <div>
+         <h1>
+            Hello World!!!
+         </h1>
+         <h2>
+            Hello World!!!
+         </h2>
+         <h3>
+            Hello World!!!
+         </h3>
+         <h4>
+            Hello World!!!
+         </h4>
+         <h5>
+            Hello World!!!
+         </h5>
+         <h6>
+            Hello World!!!
+         </h6>
+         <p>
+             A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+        </p>
+        </div>
+      );
+   }
+}
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -9,18 +9,23 @@ var config = {
        port: 8080
     },
     module: {
-       rules: [
-          {
-             test: /\.jsx?$/,
-             exclude: /node_modules/,
-             use: {
-                 loader: 'babel-loader',
-                 query: {
-                    presets: ['es2015', 'react']
-                 }
-             }
-          }
-       ]
+       rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                query: {
+                   presets: ['es2015', 'react']
+                }
+            }
+        }, {
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS
+            ]
+        }]
     }
  }
  module.exports = config;
